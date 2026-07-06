afb592a237b89fc5a223ec77000484be4ae49548
Asmad Nadeem <asmadnadeem53@gmail.com>
2026-07-06 15:22:59 +0300

    Day 2

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import pdfplumber
+
 with pdfplumber.open('test.pdf') as pdf:
+    text = ""
     for page in pdf.pages:
-        text = page.extract_text()
-        print(text)
+        text += page.extract_text()
+
+def chunk_text(text, chunk_size=500, overlap=10):
+    chunks = []
+    start = 0
+    while start < len(text):
+        chunks.append(text[start:start+chunk_size])
+        start+=chunk_size-overlap
+    return chunks
+
+result = chunk_text(text)
+
+print(len(result))
+print(result[0])
+print(result[1])
