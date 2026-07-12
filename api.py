4f8e4a4cbc5bb007a10c0408cd831669dbc9c833
Asmad Nadeem <asmadnadeem53@gmail.com>
2026-07-12 22:15:22 +0300

    error handling

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, HTTPException
 app = FastAPI()
 from sqlalchemy import create_engine, text
 engine = create_engine(os.getenv('DATABASE_URL'))
@@ -9,7 +9,14 @@ from rag_pipeline import process_pdf, answer_question
 
 @app.post("/upload")
 async def upload_pdf(file: UploadFile):
+    if not file.filename.endswith('.pdf'):
+        raise HTTPException(status_code=400, detail="Only PDF files are supported")
+    
     contents = await file.read()
+
+    if len(contents) == 0:
+        raise HTTPException(status_code=400, detail="Empty file attached")
+    
     with engine.connect() as conn:
         result = conn.execute(
         text("INSERT INTO documents (filename, file_size, status) VALUES (:filename, :file_size, :status) RETURNING id"),
@@ -20,6 +27,9 @@ async def upload_pdf(file: UploadFile):
 
     num_chunks = process_pdf(contents, document_id)
 
+    if num_chunks == 0:
+        raise HTTPException(status_code=400, detail="NO text detected on PDF")
+    
     return {
         "filename": file.filename,
         "status": "received",
@@ -28,6 +38,18 @@ async def upload_pdf(file: UploadFile):
 
 @app.post('/query')
 async def ask_question(question: str, document_id: int):
+    if question.strip() == "":
+        raise HTTPException(status_code=400, detail="Empty question")
+    
+    with engine.connect() as conn:
+        result = conn.execute(
+        text("SELECT id FROM documents WHERE id = :document_id"),
+        {"document_id": document_id}
+        )
+        document = result.fetchone()
+    if document is None:
+        raise HTTPException(status_code=404, detail="Document not found")
+
     answer = answer_question(question, document_id)
     return {
         'answer': answer
@@ -55,4 +77,3 @@ def create_tables():
         conn.commit()
 
 create_tables()
-
